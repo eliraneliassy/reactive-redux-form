b6bd3618196e6fa5fe400ad8afc966a947295f63
Eliran Eliassy <eliran@stuccomedia.com>
2018-08-14 14:59:18 +0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  
 
   methodOption = [{ label: 'affine', value: 'Affine' },
   { label: 'pixel', value: 'Pixel' },
@@ -20,21 +22,38 @@ export class AppComponent implements OnInit {
   form: FormGroup;
   steps = 0;
 
+  valueSubscription: Subscription;
+
 
   ngOnInit(): void {
-    this.form = new FormGroup({});
+    this.form = new FormGroup({
+      'methods': new FormArray([])
+    });
+
+    this.valueSubscription = this.form.valueChanges.subscribe(
+      (formValue) => {
+        console.log(formValue);
+      }
+    );
+
+    console.log(this.form);
   }
 
 
   addMethod() {
     const group = new FormGroup({
-      'method': new FormControl(null, Validators.required),
+      'methodName': new FormControl(null, Validators.required),
       'operations': new FormArray([])
     });
 
-    this.form.addControl('steps ' + this.steps++, group);
+    (<FormArray>this.form.controls['methods']).push(group);
+
 
     console.log(this.form);
 
+  }
+
+  ngOnDestroy(): void {
+    this.valueSubscription.unsubscribe();
   }
 }
